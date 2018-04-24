da22a4e7350f253855e6a11d3f3a98659335112b
Peter Schwier <peter.schwier@cgius.com>
2018-04-24 15:14:25 -0500

    updated end specs

--- src/address/end.spec.ts
+++ src/address/end.spec.ts
@@ -3,61 +3,45 @@ import { expect } from "chai";
 import { End } from "./end";
 import { Document } from '../models/document';
 
-function getDocument(text?: string): Document {
-    if (text === undefined) {
-        text = "asdf";
-    }
-    return new Document(text, []);
-}
-
 describe(End.name, () => {
     let address = new End();
 
     describe("getRangeForward function", () => {
-        it('exists', () => {
-            expect(address)
-                .has.property("getRangeForward")
-                .that.is.a("function");
+        describe("on empty string", () => {
+            let document = new Document("", []);
+            let start = 0; let end = 0;
+            it(`starts at ${start}`, () => {
+                let range = address.getRangeForward(document, 0);
+                expect(range).has.property("start").that.equals(start);
+            });
+            it(`ends at ${end}`, () => {
+                let range = address.getRangeForward(document, 0);
+                expect(range).has.property("end").that.equals(end);
+            });
         });
-        it('takes two arguments', () => {
-            expect(address)
-                .has.property("getRangeForward")
-                .has.property("length")
-                .that.equals(2);
+        describe("on one line string", () => {
+            let document = new Document("asdf", []);
+            let start = document.text.length; let end = document.text.length;
+            it(`starts at ${start}`, () => {
+                let range = address.getRangeForward(document, 0);
+                expect(range).has.property("start").that.equals(start);
+            });
+            it(`ends at ${end}`, () => {
+                let range = address.getRangeForward(document, 0);
+                expect(range).has.property("end").that.equals(end);
+            });
         });
-    });
-
-    describe("on empty string", () => {
-        let document = getDocument("");
-        it("starts at 0", () => {
-            expect(address.getRangeForward(document, 0))
-                .has.property("start").that.equals(0);
-        });
-        it("ends at 0", () => {
-            expect(address.getRangeForward(document, 0))
-                .has.property("end").that.equals(0);
-        });
-    });
-    describe("on one line string", () => {
-        let document = getDocument("asdf");
-        it(`starts at ${document.text.length}`, () => {
-            expect(address.getRangeForward(document, 0))
-                .has.property("start").that.equals(document.text.length);
-        });
-        it(`ends at ${document.text.length}`, () => {
-            expect(address.getRangeForward(document, 0))
-                .has.property("end").that.equals(document.text.length);
-        });
-    });
-    describe("on multi line string", () => {
-        let document = getDocument("asdf\nfdsa\nasdf");
-        it(`starts at ${document.text.length}`, () => {
-            expect(address.getRangeForward(document, 0))
-                .has.property("start").that.equals(document.text.length);
-        });
-        it(`ends at ${document.text.length}`, () => {
-            expect(address.getRangeForward(document, 0))
-                .has.property("end").that.equals(document.text.length);
+        describe("on multi line string", () => {
+            let document = new Document("asdf\nfdsa\nasdf", []);
+            let start = document.text.length; let end = document.text.length;
+            it(`starts at ${start}`, () => {
+                let range = address.getRangeForward(document, 0);
+                expect(range).has.property("start").that.equals(start);
+            });
+            it(`ends at ${end}`, () => {
+                let range = address.getRangeForward(document, 0);
+                expect(range).has.property("end").that.equals(end);
+            });
         });
     });
 });
